c010c98a9f1e1a1a3d86e3f4f5396dab3ea4632a
Nipasiri <sayfanacha@gmail.com>
2025-12-23 12:29:45 +0700

    lab9_1.

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -1,16 +1,52 @@
-#include <iostream> 
+#include<iostream>
+#include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-void printO(int N, int M) {
-    if (N <= 0 || M <= 0) {
-        cout << "Invalid input";
-        return;
+int main(){
+    double loan, rate, pay;
+    int year = 1;
+
+    cout << "Enter initial loan: ";
+    cin >> loan;
+
+    cout << "Enter interest rate per year (%): ";
+    cin >> rate;
+
+    cout << "Enter amount you can pay per year: ";
+    cin >> pay;
+
+    // table header
+    cout << setw(13) << left << "EndOfYear#"; 
+    cout << setw(13) << left << "PrevBalance"; 
+    cout << setw(13) << left << "Interest"; 
+    cout << setw(13) << left << "Total";
+    cout << setw(13) << left << "Payment";
+    cout << setw(13) << left << "NewBalance";
+    cout << "\n";
+    
+    cout << fixed << setprecision(2);
+
+    // loop until loan is fully paid
+    while (loan > 0) {
+        double prevBalance = loan;
+        double interest = prevBalance * rate / 100.0;
+        double total = prevBalance + interest;
+
+        // last year payment check
+        double payment = (pay > total) ? total : pay;
+        double newBalance = total - payment;
+
+        cout << setw(13) << left << year;
+        cout << setw(13) << left << prevBalance;
+        cout << setw(13) << left << interest;
+        cout << setw(13) << left << total;
+        cout << setw(13) << left << payment;
+        cout << setw(13) << left << newBalance;
+        cout << "\n";
+
+        loan = newBalance;
+        year++;
     }
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            cout << "O";
-        }
-        cout << endl;
-    }
+    return 0;
 }
